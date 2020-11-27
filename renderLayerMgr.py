dc92a1fd0865f556c95282a197cb15e1618bf869
neilk19 <67288256+neilk19@users.noreply.github.com>
2020-11-27 14:57:51 -0800

    Add files via upload

--- renderLayerMgr.py
+++ renderLayerMgr.py
@@ -9,41 +9,92 @@ from PySide2 import QtGui
 from shiboken2 import wrapInstance
 import maya.OpenMayaUI as omui
 
+
+"""
+RenderLayerGen
+
+Features:
+-Creates renderLayers retaining reflection and lighting information
+-Changes made in the tool ui is reflected in maya UI
+-Works with most renderers
+
+Code seperated into 
+
+---Main function
+
+--- UI
+    --Custom Widgets classes
+    --Main UI Class
+
+---Functions
+    --Maya's Default renderlayer functions
+    --Event handeling
+
+    --Main Class
+        - Validation Checks
+        - core functions 
+"""
+
+
+# Main Function to create Renderlayer
+def create_layer():
+    # Creates an object of RenderlayerMgr
+    rl = RenderLayerMgr()
+
+    # Validate if selected object matches requirements
+    if rl.validation_chk() == True:
+
+        # For mesh(s) in list execute following
+        for obj in rl.get_obj_transform_name():
+            # Render layer Object
+            render_layer = rl.create_render_layer(obj)
+
+            # Render layer Name
+            ren_layer_name = render_layer.name()
+
+            # Set render layer as current
+            rl.set_current_render_layer(ren_layer_name)
+
+            # Create mesh transform collection(s) 1
+            objColl1 = rl.create_collection(render_layer, 'ObjectCollection_1')
+            rl.add_obj_to_collection(obj=obj)
+
+            # Create mesh transform collection(s) 2
+            objColl2 = rl.create_collection(render_layer, 'ObjectCollection_2')
+            rl.add_obj_to_collection(obj=obj, selected=True)
+
+            # Create mesh shape collection(s)
+            shapeColl = rl.create_collection(objColl2, 'ShapeCollection', pattern='*', filterType=2)
+            rl.create_visibility_absoulte_override(shapeColl)
+
+# Get Maya's main window
 def get_maya_window():
+    '''
+    Get Maya's main window
+    '''
     main_win_ptr = omui.MQtUtil.mainWindow()
     return wrapInstance(long(main_win_ptr), QtWidgets.QWidget)
-
-# Main
-def create_layer():
-    r1 = RenderLayerMgr()
-    if r1.validation_chk() == True:
-        for obj in r1.get_obj_transform_name():
-            render_layer = r1.create_render_layer(obj)
-            ren_layer_name = render_layer.name()
-            r1.set_current_render_layer(ren_layer_name)
-            objColl1 = r1.create_collection(render_layer, 'ObjectCollection_1')
-            r1.add_obj_to_collection(obj=obj)
-            # cmds.select(cl=True)
-            objColl2 = r1.create_collection(render_layer, 'ObjectCollection_2')
-            r1.add_obj_to_collection(obj=obj, selected=True)
-            shapeColl = r1.create_collection(objColl2, 'ShapeCollection', pattern='*', filterType=2)
-            r1.create_visibility_absoulte_override(shapeColl)
 
 class GCProtector(object):
     """
     This class acts as a holder for a static class var in order to
     prevent Qt _widgets from being garbage-collected
     """
-    # declare static class var
+    #Declare static class var
     widgets = []
 
 # Custom label that switches between widgets on doubleClick mouse event
 class SwitchLabel(QtWidgets.QLabel):
     def __init__(self, switch=None, parent=None):
+        """
+        :param switch: recieves the buddy widget to switch with
+        :param parent: Widget to parent to
+        """
         super(SwitchLabel, self).__init__(parent)
         self.switch = switch
         GCProtector.widgets.append(self)
-    # When it's clicked, hide itself and show its buddy
+   
+   # Modifying in-built event to show and hide widget
     def mouseDoubleClickEvent(self, event):
         self.hide()
         self.switch.show()
@@ -57,9 +108,10 @@ class DoubleClickEditButton(QtWidgets.QPushButton):
 
         self.setContentsMargins(0, 0, 0, 0)
         self.setCheckable(True)
-        self.setFixedSize(400, 35)  # Set minimum otherwise it will collapse the container
+        self.setFixedSize(400, 35)
         self.setCheckable(True)
 
+        # Set button color and chamfer edges
         self.setStyleSheet("""
             QPushButton {
                 background-color: rgb(93, 93, 93);
@@ -79,6 +131,16 @@ class RenderItemButton(DoubleClickEditButton):
     layer_nme_label = SwitchLabel(parent=None)
 
     def __init__(self, renderlayerName='', isVisible=bool, isRenderable=bool,layerInstance=None, parent=None):
+        """
+        RenderItemButton is a custom button widget that represents a maya renderlayer in the UI
+        Inherits from DoubleClickEditButton
+
+        :param renderlayerName: Recieves renderlayer name
+        :param isVisible: If render layer is visible
+        :param isRenderable: If render layer is renderable
+        :param layerInstance: returns render layer instanced object
+        :param parent: widget parented to
+        """
         super(RenderItemButton, self).__init__(parent)
 
         self.render_layer_name = renderlayerName
@@ -88,20 +150,19 @@ class RenderItemButton(DoubleClickEditButton):
         self.jobList = []
         self.win_parent = parent
 
-
         self.create_widgets()
         self.create_layouts()
         self.create_connections()
         self.create_script_jobs()
 
     def create_widgets(self):
-
         # Line edit to rename render layer
         self.layer_nme_le = QtWidgets.QLineEdit()
         self.layer_nme_le.setStyleSheet("QLineEdit {background-color: rgb(93, 93, 93);\n border: none;}")
         self.layer_nme_le.setContentsMargins(15, 0, 0, 0)
         self.layer_nme_le.setFixedSize(150, 20)
         self.layer_nme_le.setHidden(True)
+        # Creating an event filter to catch when user double clicks button
         self.installEventFilter(self.layer_nme_le)
 
         # QLabel to display render layer name
@@ -128,6 +189,7 @@ class RenderItemButton(DoubleClickEditButton):
 
         # Delete renderlayer button
         self.delete_layer_btn = QtWidgets.QPushButton()
+        # Custom styling using qtStyleSheet
         self.delete_layer_btn.setStyleSheet("""QPushButton {
                                                   background-color: rgb(100,100,100);
                                                   border-radius: 12px;
@@ -140,6 +202,7 @@ class RenderItemButton(DoubleClickEditButton):
         self.delete_layer_btn.setIcon(QtGui.QIcon(":SP_MessageBoxCritical.png"))
 
         # Edit renderlater button
+        # Custom styling using qtStyleSheet
         self.edit_layer_btn = QtWidgets.QPushButton()
         self.edit_layer_btn.setStyleSheet("""QPushButton {
                                           background-color: rgb(100,100,100);
@@ -154,6 +217,7 @@ class RenderItemButton(DoubleClickEditButton):
         self.edit_layer_btn.setFixedSize(24, 24)
         self.edit_layer_btn.setIcon(QtGui.QIcon(":pencilCursor.png"))
 
+        # Update values whenever a new render layer is created, just incase
         self.update_values()
 
     def create_layouts(self):
@@ -192,26 +256,23 @@ class RenderItemButton(DoubleClickEditButton):
         self.set_render_layer_current_btn.pressed.connect(self.set_renLayer_renderable)
         self.delete_layer_btn.pressed.connect(self.delete_ren_layer)
 
-    def eventFilter(self, obj, event):
-        if event.type() == QtCore.QEvent.MouseButtonDblClick:
-            pass
-            # self.layer_nme_le.setFocus(QtCore.Qt.MouseFocusReason)
-
+    # Updates values in the UI
     def update_values(self):
         self.get_renLayer_vis()
         self.get_renLayer_renderable()
         self.get_renLayer_name()
-        pass
+
 
     #  Get UI Attribute values
     def get_UI_renLayer_name(self):
         text = self.layer_nme_label.text()
         return text
 
-    #  Get Attribute values
+    ### Get current Value and if doesn't match current update them.
+
+    # Get attribute values
     def get_renLayer_name(self):
         name = self.layer_instance.name()
-
         if self.render_layer_name == name:
             self.layer_nme_label.setText(self.render_layer_name)
             self.layer_nme_le.setText(self.render_layer_name)
@@ -250,9 +311,15 @@ class RenderItemButton(DoubleClickEditButton):
         if self.set_render_layer_current_btn.isChecked() == False:
            self.layer_instance.setRenderable(True)
 
+    # Function to delete renderLayer
     def delete_ren_layer(self):
         renderLayer.delete(self.layer_instance)
 
+    """
+    Create scriptjobs
+    These jobs are attached to a named condition, event, or attribute.
+    Each time the condition switches to the desired state (or the trigger is triggered, etc), the script is run.
+    """
     def create_script_jobs(self):
         self.kill_scriptJobs()
         nodeNameChange = cmds.scriptJob(nodeNameChanged=['rs_{0}'.format(self.render_layer_name), self.update_values])
@@ -265,14 +332,19 @@ class RenderItemButton(DoubleClickEditButton):
 
 # Main Ui
 class RenLayerManagerUI(QtWidgets.QDialog):
-
+    """
+    RenLayerManagerUI inherits from QDialog.
+    This Window is parented to the main maya window contains all the main widgets that makes up the UI
+    """
     WIN_TITLE = 'RenderLayerGen'
 
     def __init__(self, parent=get_maya_window()):
         super(RenLayerManagerUI, self).__init__(parent)
 
+
         self.rs_functions = RenderLayerMgr()
 
+        #Setting QDialog parameters
         self.setWindowTitle(self.WIN_TITLE)
         self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
         self.setMinimumHeight(250)
@@ -283,20 +355,27 @@ class RenLayerManagerUI(QtWidgets.QDialog):
         self.create_layouts()
         self.create_connections()
 
+        # Script jobs lists
         self.jobList = []
+        # List of instanced objects of RenderItemButton Class
         self.ren_item_list = []
+        # Creating Instance of rendersetup
         self.render_setup = renderSetup.instance()
 
         self.show()
 
     def create_widgets(self):
+
+        # Creating a container widget
         self.containter_wdg = QtWidgets.QWidget()
 
+        #Tip label
         self.tip_text_label = QtWidgets.QLabel("First select 'mesh' object(s) you want to isolate")
         self.tip_text_label.setFont(QtGui.QFont("Times", 7, QtGui.QFont.Bold))
         self.tip_text_label.setAlignment(QtCore.Qt.AlignHCenter)
         self.tip_text_label.setFixedHeight(15)
 
+        #Create render layer button
         self.create_ren_layer_btn = QtWidgets.QPushButton('Create render layer')
         self.create_ren_layer_btn.setFixedWidth(200)
         self.create_ren_layer_btn.setFixedHeight(30)
@@ -312,7 +391,7 @@ class RenLayerManagerUI(QtWidgets.QDialog):
                                                 "background-color: rgba(93, 120, 93);"
                                                 "border-radius: 4px;}")
 
-
+        # Set to default render layer button
         self.defaultRenderLayer_btn = QtWidgets.QPushButton('Return to DefaultRenderLayer')
         self.defaultRenderLayer_btn.setFixedWidth(200)
         self.defaultRenderLayer_btn.setFixedHeight(30)
@@ -325,24 +404,29 @@ class RenLayerManagerUI(QtWidgets.QDialog):
                                                     "border-radius: 4px;}")
 
     def create_layouts(self):
+        # Using a QButtonGroup works as a radio button so I cans select the layers one at a time only
         self.layer_selection_btn_group = QtWidgets.QButtonGroup()
         self.layer_vis_btn_group = QtWidgets.QButtonGroup()
 
+        # Creating a button layout to contain Button group
         self.create_btn_layout = QtWidgets.QHBoxLayout()
         self.create_btn_layout.setContentsMargins(0,0,0,0)
         self.create_btn_layout.setAlignment(QtCore.Qt.AlignHCenter)
         self.create_btn_layout.addWidget(self.create_ren_layer_btn)
         self.create_btn_layout.addWidget(self.defaultRenderLayer_btn)
 
+        # Creating a container widget to hold the scroll list
         self.ren_layer_list_layout = QtWidgets.QVBoxLayout(self.containter_wdg)
         self.ren_layer_list_layout.setContentsMargins(10, 10, 10, 10)
         self.ren_layer_list_layout.setSpacing(3)
         self.ren_layer_list_layout.setAlignment(QtCore.Qt.AlignTop)
 
+        #Creating a scroll area to scroll through the layer list
         self.light_list_scroll = QtWidgets.QScrollArea()
         self.light_list_scroll.setWidgetResizable(True)
         self.light_list_scroll.setWidget(self.containter_wdg)
 
+        # Main layout brings all the container layout together into one mainlayout
         self.mainlayout = QtWidgets.QVBoxLayout(self)
         self.mainlayout.setContentsMargins(0, 10, 0, 0)
         self.mainlayout.addWidget(self.tip_text_label)
@@ -366,12 +450,16 @@ class RenLayerManagerUI(QtWidgets.QDialog):
                 'if cmds.scriptJob(exists={0}): \t cmds.scriptJob(kill={0}, force=True)'.format(job_number))
         self.script_jobs = []
 
+    # REDUNDANT FUNCTION
     def is_more_than_one_obj(self):
         ren_manager_inst = RenderLayerMgr()
         obj_list = ren_manager_inst.get_obj_transform_name()
         return obj_list
 
+    # Refresh RenderItemButton item in the scroll list
     def refresh_values(self):
+
+        # CLears all items and adds updated list
         self.clear_items()
         self.render_layers = self.render_setup.getRenderLayers()
         for render_layer in self.render_layers:
@@ -390,16 +478,17 @@ class RenLayerManagerUI(QtWidgets.QDialog):
             self.layer_vis_btn_group.addButton(self.render_layer_item.set_render_layer_vis_btn)
             self.ren_layer_list_layout.addWidget(self.render_layer_item)
 
+    # Killing script Jobs before creating new ones is IMPERITIVE!
     def clear_items(self):
         for item in self.ren_item_list:
             item.kill_scriptJobs()
             self.ren_item_list = []
 
-        # Clearing the ligh list
+        # Clearing the renderLayer list
         while self.ren_layer_list_layout.count() > 0:
-            light_item = self.ren_layer_list_layout.takeAt(0)
-            if light_item.widget():
-                light_item.widget().deleteLater()
+            layer_item = self.ren_layer_list_layout.takeAt(0)
+            if layer_item.widget():
+                layer_item.widget().deleteLater()
 
     def get_renlayer_list(self):
         render_layer_names = self.render_setup.getRenderLayers()
@@ -439,6 +528,7 @@ class RenLayerManagerUI(QtWidgets.QDialog):
 #   Event functions
 ##########################################
 
+    # These fucntions are executed when the built-in show and event is executed.
     def showEvent(self, event):
         self.create_scriptJobs()
         self.refresh_values()
@@ -614,6 +704,7 @@ class RenderLayerMgr(object):
     def set_current_render_layer(self, renLayer):
         cmds.editRenderLayerGlobals(currentRenderLayer='rs_{0}'.format(renLayer))
 
+# Show UI
 try:
     ren_mgr.close()
     ren_mgr.deleteLater()
